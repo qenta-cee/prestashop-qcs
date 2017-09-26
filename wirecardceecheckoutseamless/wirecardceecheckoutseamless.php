760556cfcaaad502bf273c69ffcf951b03ae86d4
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-09-26 12:38:05 +0200

    #21 Rename of Sofort banking

--- wirecardceecheckoutseamless/wirecardceecheckoutseamless.php
+++ wirecardceecheckoutseamless/wirecardceecheckoutseamless.php
@@ -620,11 +620,11 @@ class WirecardCEECheckoutSeamless extends PaymentModule
                     ),
                     array(
                         'name' => 'sofortbanking',
-                        'label' => $this->l('SOFORT Banking'),
+                        'label' => $this->l('Online bank transfer.'),
                         'type' => 'onoff',
                         'default' => 1,
                         'class' => 'Sofortbanking',
-                        'logo' => 'sofortbanking-%s.png'
+                        'logo' => 'sofortbanking.png'
                     ),
                     array(
                         'name' => 'paypal',

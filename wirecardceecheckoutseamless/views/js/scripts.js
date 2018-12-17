f40f6dea4474a9bb1ff9a8ecfc3763f265d787c5
Mihael Lovrencic <mihael.lovrencic@wirecard.com>
2018-12-17 16:45:25 +0100

    #31 update all submit buttons

--- wirecardceecheckoutseamless/views/js/scripts.js
+++ wirecardceecheckoutseamless/views/js/scripts.js
@@ -49,10 +49,10 @@ function checkConditions() {
   $("#conditions-to-approve input[type=checkbox]").each(function(){
     $(this).on("change",function(){
       if($(this).is(":checked")) {
-        $(".js-payment-option-form:visible button:visible").prop("disabled",false);
+        $(".js-payment-option-form button[type=submit]").prop("disabled",false);
       }
       else {
-        $(".js-payment-option-form:visible button:visible").prop("disabled",true);
+        $(".js-payment-option-form button[type=submit]").prop("disabled",true);
         return false;
       }
     });
